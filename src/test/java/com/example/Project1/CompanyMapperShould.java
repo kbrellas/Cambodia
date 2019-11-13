f325ee8ac3014a3b48ac5a59b04a9e40e8e3ea0d
k-gard <std080064@ac.eap.gr>
2019-11-13 19:18:49 +0200

    Implemented CompanyMapper unit tests

---
+++ src/test/java/com/example/Project1/CompanyMapperShould.java
@@ -0,0 +1,37 @@
+package com.example.Project1;
+
+import com.example.Project1.mappers.CompanyMapper;
+import com.example.Project1.models.Company;
+import com.example.Project1.models.CompanyResponse;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CompanyMapperShould {
+
+    private Company companyInput;
+    private CompanyResponse companyResponseOutput;
+
+
+    @Before
+    public void setup(){
+        CompanyMapper mapper=new CompanyMapper();
+    companyInput=new Company("test Company");
+    companyInput.setId(111);
+    companyResponseOutput=mapper.mapCompanyToCompanyResponse(companyInput);
+    }
+
+    @Test
+    public void keepSameName(){
+        Assert.assertEquals(companyInput.getName(),companyResponseOutput.getName());
+
+    }
+
+    @Test
+    public void keepSameId(){
+        Assert.assertEquals(companyInput.getId(),companyResponseOutput.getId());
+
+    }
+
+}
