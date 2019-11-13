a4ca11fbddc3927f5ac8c1f79d6c35f8a05a8891
k-gard <std080064@ac.eap.gr>
2019-11-13 19:11:11 +0200

    Changed getAllCompanies() method return type to ResponseEntity

--- src/main/java/com/example/Project1/controllers/CompanyController.java
+++ src/main/java/com/example/Project1/controllers/CompanyController.java
@@ -1,8 +1,9 @@
 package com.example.Project1.controllers;
 
-import com.example.Project1.models.GetAllCompanyResponses;
 import com.example.Project1.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,9 @@ public class CompanyController {
     private CompanyService service;
 
     @GetMapping("/allCompanies")
-    public GetAllCompanyResponses getAllCompanies(){
-        return new GetAllCompanyResponses(service.getAllCompanies());
+    public ResponseEntity getAllCompanies() {
+        if (service.getAllCompanies().getError()!= null) {
+            return new ResponseEntity<>(service.getAllCompanies().getError(), null, HttpStatus.OK); }
+        return new ResponseEntity<>(service.getAllCompanies().getData(), null, HttpStatus.OK);
     }
 }
