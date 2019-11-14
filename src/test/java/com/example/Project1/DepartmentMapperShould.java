bdad96496fc3680563a7a80d98782d6369349b11
sofie-lu <sofie.mpl@gmail.com>
2019-11-14 18:06:53 +0200

    Add unit test for DepartmentMapper

---
+++ src/test/java/com/example/Project1/DepartmentMapperShould.java
@@ -0,0 +1,36 @@
+package com.example.Project1;
+
+import com.example.Project1.mappers.DepartmentMapper;
+import com.example.Project1.models.BusinessUnit;
+import com.example.Project1.models.Department;
+import com.example.Project1.models.DepartmentResponse;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DepartmentMapperShould {
+
+    private DepartmentMapper mapper;
+    private Department input;
+    private DepartmentResponse output;
+    private BusinessUnit businessUnit;
+
+    @Before
+    public void setup() {
+        mapper = new DepartmentMapper();
+        businessUnit = new BusinessUnit("BusinessUnit1");
+        businessUnit.setId(1);
+        input = new Department("HR1",businessUnit);
+        output = mapper.mapDepartmentToDepartmentResponse(input);
+    }
+
+    @Test
+    public void keepSameName() {
+        Assert.assertEquals(input.getName(), output.getName());
+    }
+
+    @Test
+    public void keepSameBusinessUnit() {
+        Assert.assertEquals(input.getBusinessUnit().getName(),output.getBusinessUnitName());
+    }
+}
