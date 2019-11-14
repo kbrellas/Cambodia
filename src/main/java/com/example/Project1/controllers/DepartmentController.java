e46c7bd03400530e4388bc39cd28f8f708fe91e2
sofie-lu <sofie.mpl@gmail.com>
2019-11-14 17:29:09 +0200

    Convert return type to ResponseEntity in getAllDepartments() and add get DepartmentByCriteria() in DepartmentController, Convert return type to GenericResponse in getAllDepartments and add get DepartmentByCriteria() in DepartmentService

--- src/main/java/com/example/Project1/controllers/DepartmentController.java
+++ src/main/java/com/example/Project1/controllers/DepartmentController.java
@@ -1,9 +1,17 @@
 package com.example.Project1.controllers;
+import com.example.Project1.models.DepartmentResponse;
+import com.example.Project1.models.GenericResponse;
 import com.example.Project1.models.GetAllDepartmentResponses;
 import com.example.Project1.services.DepartmentService;
+import com.example.Project1.models.Error;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -12,8 +20,41 @@ public class DepartmentController {
     private DepartmentService service;
 
     @GetMapping("/allDepartments")
-    public GetAllDepartmentResponses getAllDepartments() {
-        return new GetAllDepartmentResponses(service.getAllDepartments());
+    public ResponseEntity getAllDepartments() {
+        GenericResponse<List<DepartmentResponse>> response = service.getAllDepartments();
+            if(response.getError() != null)
+                return new ResponseEntity(
+                        response.getError(),
+                        null,
+                        HttpStatus.INTERNAL_SERVER_ERROR
+                );
+
+            return new ResponseEntity(
+                    response.getData(),
+                    null,
+                    HttpStatus.OK);
     }
 
+    @GetMapping("/getDepartmentsByCriteria/{criteria}/{criteriaId}")
+    public ResponseEntity getDepartmentsByCriteria(@PathVariable String criteria,
+                                             @PathVariable Long criteriaId) {
+        if(!criteria.equals("businessUnit"))
+            return new ResponseEntity(
+                    new Error(0, "Wrong Criteria", "The criteria input should be businessUnit"),
+                    null,
+                    HttpStatus.BAD_REQUEST
+            );
+        GenericResponse<List<DepartmentResponse>> response = service.getDepartmentsByCriteria(criteria, criteriaId);
+        if(response.getError() != null)
+            return new ResponseEntity(
+                    response.getError(),
+                    null,
+                    HttpStatus.BAD_REQUEST
+            );
+
+        return new ResponseEntity(
+                new GetAllDepartmentResponses(response.getData()),
+                null,
+                HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/Project1/services/DepartmentService.java
+++ src/main/java/com/example/Project1/services/DepartmentService.java
@@ -1,11 +1,14 @@
 package com.example.Project1.services;
 
 import com.example.Project1.mappers.DepartmentMapper;
+import com.example.Project1.models.GenericResponse;
+import com.example.Project1.repositories.BusinessUnitRepository;
 import com.example.Project1.repositories.DepartmentRepository;
 import com.example.Project1.models.Department;
 import com.example.Project1.models.DepartmentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.Project1.models.Error;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,15 +22,40 @@ public class DepartmentService {
     @Autowired
     private DepartmentRepository repository;
 
-    public List<DepartmentResponse> getAllDepartments(){
-        Iterable<Department> retrievedDepartments= repository.findAll();
-        List<DepartmentResponse> departments = new ArrayList<>();
-        for (Department department: retrievedDepartments
-             ) {
-            departments.add(mapper.mapDepartmentToDepartmentResponse(department));
+    @Autowired
+    private BusinessUnitRepository businessUnitRepository;
+
+    public GenericResponse<List<DepartmentResponse>> getAllDepartments(){
+       try {
+            Iterable<Department> retrievedDepartments = repository.findAll();
+            List<DepartmentResponse> departments = new ArrayList<>();
+            for (Department department : retrievedDepartments) {
+                departments.add(mapper.mapDepartmentToDepartmentResponse(department));
+            }
+            if (departments.isEmpty()) {
+                return new GenericResponse<>(new Error(0, "Empty Repository", "Please add Department"));
+            }
+            return new GenericResponse<>(departments);
+        } catch(Exception e){
+            e.printStackTrace();
+            return new GenericResponse<>(new Error(0,"Internal Server Error", "Unable to retrieve data"));
         }
-        return departments;
     }
 
-
+    public GenericResponse<List<DepartmentResponse>> getDepartmentsByCriteria(String criteria, Long criteriaId) {
+        Iterable<Department> departments = repository.findAll();
+        List<DepartmentResponse> departmentsToReturn = new ArrayList<>();
+        if (criteria.equals("businessUnit")) {
+            if (!businessUnitRepository.findById(criteriaId).isPresent()) {
+                return new GenericResponse<>(new Error(0, "Wrong Input", "BusinessUnit with id " + criteriaId + " does not exist"));
+            }
+            for (Department department : departments) {
+                if (department.getBusinessUnit().getId() == criteriaId) {
+                    departmentsToReturn.add(mapper.mapDepartmentToDepartmentResponse(department));
+                }
+            }
+        }
+        return new GenericResponse<>(departmentsToReturn);
+    }
 }
+
