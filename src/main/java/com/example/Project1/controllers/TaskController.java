f429993473b4e1b8f90e588b8405622285160fac
DMazarakis <dmazarak@gmail.com>
2019-11-19 01:06:03 +0200

    Added TaskController /findTaskById/{taskId} end point

--- src/main/java/com/example/Project1/controllers/TaskController.java
+++ src/main/java/com/example/Project1/controllers/TaskController.java
@@ -1,13 +1,17 @@
 package com.example.Project1.controllers;
 
 import com.example.Project1.models.EmployeeResponse;
+import com.example.Project1.models.Error;
 import com.example.Project1.models.GenericResponse;
 import com.example.Project1.models.TaskResponse;
 import com.example.Project1.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,6 +38,36 @@ public class TaskController {
                 null,
                 HttpStatus.OK
         );
+    }
+    @GetMapping("/findTaskById/{taskId}")
+    public ResponseEntity getTaskById(@PathVariable long taskId){
+        GenericResponse<TaskResponse> response = service.getTaskById(taskId);
+        if (response.getError()!=null){
+            return new ResponseEntity<>(
+                    response.getError(),
+                    null,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return new ResponseEntity<>(
+                response.getData(),
+                null,
+                HttpStatus.OK
+        );
+
+
+    }
+
+    @ExceptionHandler({NumberFormatException.class})
+    public ResponseEntity handleNumberFormatException(){
+        return new ResponseEntity<>(new Error(0,"Wrong input type", "Id must be type long"),
+                null,
+                HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity handleHttpMessageNotReadableException(){
+        return new ResponseEntity<>(new Error(0,"Wrong input type(s) given","Please try again"),null,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
