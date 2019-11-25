4623aade276c3f4a38f2fa0edcb4c29cce89ee98
k-gard <std080064@ac.eap.gr>
2019-11-25 11:10:40 +0200

    implemented BusinessUnitController Unit test

---
+++ src/test/java/com/example/Project1/controllers/BusinessUnitControllerShould.java
@@ -0,0 +1,68 @@
+package com.example.Project1.controllers;
+
+import com.example.Project1.models.BusinessUnitResponse;
+import com.example.Project1.models.Error;
+import com.example.Project1.models.GenericResponse;
+import com.example.Project1.services.BusinessUnitService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.when;
+
+public class BusinessUnitControllerShould {
+    private BusinessUnitController controller;
+
+    @Mock
+    private BusinessUnitService service;
+
+    @Mock
+    private BusinessUnitResponse bu1;
+    @Mock
+    private BusinessUnitResponse bu2;
+    @Mock
+    private BusinessUnitResponse bu3;
+
+    @Before
+    public  void setup(){
+        MockitoAnnotations.initMocks(this);
+        List<BusinessUnitResponse> mockedCompanyResponseList = new ArrayList<>();
+        mockedCompanyResponseList.add(bu1);
+        mockedCompanyResponseList.add(bu2);
+        mockedCompanyResponseList.add(bu3);
+        GenericResponse<List<BusinessUnitResponse>> serviceResponse=new GenericResponse<>(mockedCompanyResponseList);
+        when(service.getAllBusinessUnits()).thenReturn(serviceResponse);
+        controller=new BusinessUnitController(service);
+    }
+    @Test
+    public void returnAllCompanies(){
+        ResponseEntity<List<BusinessUnitResponse>> actual=controller.getAllBusinessUnits();
+        Assert.assertThat(actual.getBody(),hasItems(bu1,bu2,bu3));
+        Assert.assertEquals(HttpStatus.OK,actual.getStatusCode());
+    }
+
+    @Test
+    public void returnErrorWhenAllCompaniesServiceFails(){
+        Error error=MockServiceFailure();
+        ResponseEntity actual= controller.getAllBusinessUnits();
+        Assert.assertEquals(error,actual.getBody());
+        Assert.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,actual.getStatusCode());
+    }
+
+
+    private Error MockServiceFailure() {
+        Error error = new Error(0,"Error","Something went wrong");
+        when(service.getAllBusinessUnits()).thenReturn(new GenericResponse<>(error));
+        return error;
+    }
+
+
+}
