2b9695ebfd25f24c44c873619bd4e4199cda7d70
k-gard <std080064@ac.eap.gr>
2019-11-25 11:37:46 +0200

    implemented DepartmentController Unit test

--- src/main/java/com/example/Project1/controllers/DepartmentController.java
+++ src/main/java/com/example/Project1/controllers/DepartmentController.java
@@ -1,14 +1,10 @@
 package com.example.Project1.controllers;
 import com.example.Project1.models.DepartmentResponse;
 import com.example.Project1.models.GenericResponse;
-import com.example.Project1.models.GetAllDepartmentResponses;
 import com.example.Project1.services.DepartmentService;
-import com.example.Project1.models.Error;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,20 +12,24 @@ import java.util.List;
 @RestController
 public class DepartmentController {
 
-    @Autowired
+
     private DepartmentService service;
+
+    public DepartmentController(DepartmentService service) {
+        this.service = service;
+    }
 
     @GetMapping("/allDepartments")
     public ResponseEntity getAllDepartments() {
         GenericResponse<List<DepartmentResponse>> response = service.getAllDepartments();
             if(response.getError() != null)
-                return new ResponseEntity(
+                return new ResponseEntity<>(
                         response.getError(),
                         null,
                         HttpStatus.INTERNAL_SERVER_ERROR
                 );
 
-            return new ResponseEntity(
+            return new ResponseEntity<>(
                     response.getData(),
                     null,
                     HttpStatus.OK);
